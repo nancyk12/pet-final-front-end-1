4f0e9fd53610e122f22dcedf879a27eae6f0e176
nancyk12 <nancyk12@hotmail.com>
2023-04-24 00:46:08 -0400

    add icons to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,8 @@
 //import React from "react";
+import { FaMapMarkerAlt,FaBirthdayCake, FaRegHeart, FaHeart, FaShareAlt } from 'react-icons/fa';
 import {Link} from "react-router-dom";
+import { IoMaleFemale } from "react-icons/io5";
+import { TbDog } from "react-icons/tb"
 //import { useEffect, useState } from "react";
 //import { Client } from "@petfinder/petfinder-js";
 //import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,7 +17,7 @@ const Cards = (props) => {
    
   const {animal} = props;
 
-
+const style = { color:"orange", fontSize: "1rem"}
         
 return (
 
@@ -39,17 +42,22 @@ return (
               alt=""
     />
   </div>
+
   <div className="card__body">
-    <Link to={"/profile/" + animal.id}>
+    <Link to={`/animal/ ${animal.id}`}>
       <h3 className="card__title">{animal.name}</h3>
      </Link >
-      <p className="card__price">{animal.species}</p>
-      <p className="card__description"> {animal.breeds.primary}</p>
-      <p className="card__description">{animal.age}  •  {animal.gender}</p>
+      <span className="card__price">{animal.species}</span>
+      <br/>
+      <span className="card__description"> <TbDog color="orange" fontSize="1.4rem"/> {animal.breeds.primary}</span>
+      <br/>
+      <span className="card__description"><FaBirthdayCake color="orange" fontSize="1.2rem"/> {animal.age}  •  <IoMaleFemale color="orange" fontSize="1.2rem"/> {animal.gender}</span>
       {/* <p>Color: {animal.colors.primary}</p> */}
-      <p>{animal.contact.address.city}, {animal.contact.address.state} </p>
+      <br/>
+      <span className="card__location">&#128204; <FaMapMarkerAlt color="orange" fontSize="1.2rem"/> {animal.contact.address.city}, {animal.contact.address.state} </span>
       {/* <p>About {animal.name}: {animal.description}</p> */}
       <button className="card__btn">Pet Profile</button>
+      <h4><FaHeart/> <FaShareAlt/></h4>
   </div>
 </div>   
 </div> 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,50 +10,52 @@ import { Link } from 'react-router-dom';
 
 const Profile = (props) => {
   const {animal} = props;
-  const params = useParams()
 
-  const animalId = animal.find((animal) =>{
-    return animal.Name === params.title
-  })
+}
+//   const params = useParams()
+
+//   const animalId = animal.find((animal) =>{
+//     return animal.Name === params.title
+//   })
 
 
-return (
-  <div className="pet"> 
-<div key={animalId.id} >
-{/* <div style={{
-						border: "1px solid",
-						height: "400px",
-						width: "200px",
-						margin: "20px",
-            overflow: "hidden",
-					}}> */}
-  <div className="pet-image-container">
-    <img
-      className="pet-image"
-        src={
-              animalId.photos[0]?.medium || 'image/pet-photo-shoot.jpeg'
-            }
-              alt=""
-    />
-  </div>
+// return (
+//   <div className="pet"> 
+// <div key={animalId.id} >
+// {/* <div style={{
+// 						border: "1px solid",
+// 						height: "400px",
+// 						width: "200px",
+// 						margin: "20px",
+//             overflow: "hidden",
+// 					}}> */}
+//   <div className="pet-image-container">
+//     <img
+//       className="pet-image"
+//         src={
+//               animalId.photos[0]?.medium || 'image/pet-photo-shoot.jpeg'
+//             }
+//               alt=""
+//     />
+//   </div>
     
-      <h3 >{animalId.Name}</h3>
+//       <h3 >{animalId.Name}</h3>
      
-      <p>Type: {animal.type}</p>
-      <p>{animal.contact.address.city},{animal.contact.address.state} </p>
-      <p>Breed: {animal.breeds.primary}</p>
-      {/* <p>Color: {animal.colors.primary}</p> */}
-      <p>Gender: {animal.gender}</p>
-      <p>Age: {animal.age}</p>
-      {/* <p>About {animal.name}: {animal.description}</p> */}
+//       <p>Type: {animal.type}</p>
+//       <p>{animal.contact.address.city},{animal.contact.address.state} </p>
+//       <p>Breed: {animal.breeds.primary}</p>
+//       {/* <p>Color: {animal.colors.primary}</p> */}
+//       <p>Gender: {animal.gender}</p>
+//       <p>Age: {animal.age}</p>
+//       {/* <p>About {animal.name}: {animal.description}</p> */}
   
-  </div>
-</div>   
+//   </div>
+// </div>   
 
 
 
    
- );};
+//  );};
       
 
 
